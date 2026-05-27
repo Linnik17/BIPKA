84c299815300ab6411342ece841bc63b9a715a18
Linnik17 <163736722+Linnik17@users.noreply.github.com>
2026-05-27 14:36:03 +0500

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,8 +1,8 @@
 history = []
 
-def add_value(value: float):
-    history.append(value)
-    if len(history) > 300:
+def add_value(v):
+    history.append(v)
+    if len(history) > 200:
         history.pop(0)
 
 def get_history():
